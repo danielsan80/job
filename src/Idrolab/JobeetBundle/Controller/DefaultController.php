2b1694702eaed29bb268dfe7581452600815ae0e
marina <mbaldantoni@gmail.com>
2015-04-24 09:54:15 +0200

    correzione routing job

--- src/Idrolab/JobeetBundle/Controller/DefaultController.php
+++ src/Idrolab/JobeetBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->redirect('job');
+        return $this->redirect($this->generateUrl('job'));
     }
 }
